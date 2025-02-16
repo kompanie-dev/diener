709ea727bcf9f62d1d83f2748e35ee1cc9ae0ada
Fabian <heuberf@outlook.com>
2025-02-16 17:31:00 +0100

    Fix defaultMimeTypeMap.json path

--- source/argumentParser.js
+++ source/argumentParser.js
@@ -1,5 +1,11 @@
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
 export class ArgumentParser {
 	static parse(cliArguments) {
+		const currentFileName = fileURLToPath(import.meta.url);
+		const currentDirectory = dirname(currentFileName);
+
 		const args = Object.fromEntries(
 			cliArguments
 				.slice(2)
@@ -11,7 +17,7 @@ export class ArgumentParser {
 		return {
 			enableLiveReload: "--livereload" in args,
 			indexFile: args["--index"] || "index.html",
-			mimeTypeMapFilePath: args["--mimemap"] || "./source/defaultMimeTypeMap.json",
+			mimeTypeMapFilePath: args["--mimemap"] || resolve(currentDirectory, "defaultMimeTypeMap.json"),
 			port: Number.parseInt(args["--port"], 10) || 8000,
 			webServerFolder: args["--folder"] || process.cwd(),
 		};
